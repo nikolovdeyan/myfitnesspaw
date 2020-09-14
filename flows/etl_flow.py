d6c7f15d7d18ae590b72ff490e7b169402f60f34
Deyan Nikolov <deyan.nikolov@gmail.com>
2020-09-15 02:13:09 +0300

    Refactor etl_flow -> extract sql statements, secrets from env.

--- flows/etl_flow.py
+++ flows/etl_flow.py
@@ -9,117 +9,28 @@ from datetime import timedelta
 
 import jsonpickle
 import myfitnesspal
+import sql
 from prefect import Flow, Parameter, task, unmapped
 from prefect.tasks.database.sqlite import SQLiteScript
-
-create_raw_day_table_command = """
-CREATE TABLE IF NOT EXISTS RawDayData (
-  userid text NOT NULL,
-  date text NOT NULL,
-  rawdaydata json,
-  PRIMARY KEY(userid, date)
-);
-"""
-
-create_meals_table_command = """
-CREATE TABLE IF NOT EXISTS Meals (
-  userid text NOT NULL,
-  date text NOT NULL,
-  name text NOT NULL,
-  calories INTEGER,
-  carbs INTEGER,
-  fat INTEGER,
-  protein INTEGER,
-  sodium INTEGER,
-  sugar INTEGER,
-  PRIMARY KEY(userid, date, name),
-  CONSTRAINT fk_rawdaydata
-    FOREIGN KEY (userid, date)
-    REFERENCES RawDayData(userid, date)
-    ON DELETE CASCADE
-);
-"""
-
-create_mealentries_table_command = """
-CREATE TABLE IF NOT EXISTS MealEntries (
-  id integer PRIMARY KEY AUTOINCREMENT,
-  userid text NOT NULL,
-  date text NOT NULL,
-  meal_name text NOT NULL,
-  short_name text NOT NULL,
-  quantity REAL NOT NULL,
-  unit text NOT NULL,
-  calories INTEGER,
-  carbs INTEGER,
-  fat INTEGER,
-  protein INTEGER,
-  sodium INTEGER,
-  sugar INTEGER,
-  CONSTRAINT fk_mealentries
-    FOREIGN KEY(userid, date, meal_name)
-    REFERENCES Meals(userid, date, name)
-    ON DELETE CASCADE
-);
-"""
-
-create_cardioexercises_table_command = """
-CREATE TABLE IF NOT EXISTS CardioExercises (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  userid TEXT NOT NULL,
-  date TEXT NOT NULL,
-  exercise_name TEXT NOT NULL,
-  minutes REAL,
-  calories_burned REAL,
-  CONSTRAINT fk_rawdaydata
-    FOREIGN KEY(userid, date)
-    REFERENCES RawDayData(userid, date)
-    ON DELETE CASCADE
-);
-"""
-
-create_strengthexercises_table_command = """
-CREATE TABLE IF NOT EXISTS StrengthExercises (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  userid TEXT NOT NULL,
-  date TEXT NOT NULL,
-  exercise_name TEXT NOT NULL,
-  sets REAL,
-  reps REAL,
-  weight REAL,
-  CONSTRAINT fk_rawdaydata
-    FOREIGN KEY(userid, date)
-    REFERENCES RawDayData(userid, date)
-    ON DELETE CASCADE
-);
-"""
-
-create_measurements_table_command = """
-CREATE TABLE IF NOT EXISTS Measurements (
-  userid text NOT NULL,
-  date text NOT NULL,
-  measure_name text NOT NULL,
-  value REAL NOT NULL,
-  PRIMARY KEY(userid, date, measure_name)
-);
-"""
+from prefect.tasks.secrets import EnvVarSecret
 
 create_mfp_database_script = f"""
-{create_raw_day_table_command}
+{sql.create_raw_day_table}
 
-{create_meals_table_command}
+{sql.create_meals_table}
 
-{create_mealentries_table_command}
+{sql.create_mealentries_table}
 
-{create_cardioexercises_table_command}
+{sql.create_cardioexercises_table}
 
-{create_strengthexercises_table_command}
+{sql.create_strengthexercises_table}
 
-{create_measurements_table_command}
+{sql.create_measurements_table}
 """
 
 create_mfp_database = SQLiteScript(
-    name="Create MFP DB (if not existing)",
-    db="database/mfp.db",  # TODO: Improve connection string passing
+    name="<MyFitnessPaw>: Create DB (if not existing)",
+    db="database/mfp_db.sqlite",
     script=create_mfp_database_script,
 )
 
@@ -151,13 +62,14 @@ def get_myfitnesspal_day_serialized(username, password, date):
 
 
 with Flow("Myfitnesspal Prototype Flow") as flow:
-    #  Gather required parameters at runtime:
+    #  Gather required parameters/secrets
     from_date = Parameter(name="from_date", required=True)
     to_date = Parameter(name="to_date", required=True)
-    username = Parameter(name="username", required=True)
-    password = Parameter(name="password", required=True)
+    username = EnvVarSecret("MYFITNESSPAW_USERNAME", raise_if_missing=True)
+    password = EnvVarSecret("MYFITNESSPAW_PASSWORD", raise_if_missing=True)
 
     #  Prepeare a list of dates to be scraped:
+    # TODO: Should this really be a Task? Or just construct a list comprehension
     dates_to_scrape = prepare_list_of_dates_to_scrape(
         from_date, to_date, upstream_tasks=[create_mfp_database]
     )
@@ -173,6 +85,4 @@ if __name__ == "__main__":
     flow.run(
         from_date=datetime.date(2020, 9, 10),
         to_date=datetime.date(2020, 9, 10),
-        username="",
-        password="",
     )
---
+++ flows/sql.py
@@ -0,0 +1,92 @@
+"""SQL code for the Myfitnesspaw flows."""
+
+create_raw_day_table = """
+CREATE TABLE IF NOT EXISTS RawDayData (
+  userid text NOT NULL,
+  date text NOT NULL,
+  rawdaydata json,
+  PRIMARY KEY(userid, date)
+);
+"""
+
+create_meals_table = """
+CREATE TABLE IF NOT EXISTS Meals (
+  userid text NOT NULL,
+  date text NOT NULL,
+  name text NOT NULL,
+  calories INTEGER,
+  carbs INTEGER,
+  fat INTEGER,
+  protein INTEGER,
+  sodium INTEGER,
+  sugar INTEGER,
+  PRIMARY KEY(userid, date, name),
+  CONSTRAINT fk_rawdaydata
+    FOREIGN KEY (userid, date)
+    REFERENCES RawDayData(userid, date)
+    ON DELETE CASCADE
+);
+"""
+
+create_mealentries_table = """
+CREATE TABLE IF NOT EXISTS MealEntries (
+  id integer PRIMARY KEY AUTOINCREMENT,
+  userid text NOT NULL,
+  date text NOT NULL,
+  meal_name text NOT NULL,
+  short_name text NOT NULL,
+  quantity REAL NOT NULL,
+  unit text NOT NULL,
+  calories INTEGER,
+  carbs INTEGER,
+  fat INTEGER,
+  protein INTEGER,
+  sodium INTEGER,
+  sugar INTEGER,
+  CONSTRAINT fk_mealentries
+    FOREIGN KEY(userid, date, meal_name)
+    REFERENCES Meals(userid, date, name)
+    ON DELETE CASCADE
+);
+"""
+
+create_cardioexercises_table = """
+CREATE TABLE IF NOT EXISTS CardioExercises (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userid TEXT NOT NULL,
+  date TEXT NOT NULL,
+  exercise_name TEXT NOT NULL,
+  minutes REAL,
+  calories_burned REAL,
+  CONSTRAINT fk_rawdaydata
+    FOREIGN KEY(userid, date)
+    REFERENCES RawDayData(userid, date)
+    ON DELETE CASCADE
+);
+"""
+
+create_strengthexercises_table = """
+CREATE TABLE IF NOT EXISTS StrengthExercises (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userid TEXT NOT NULL,
+  date TEXT NOT NULL,
+  exercise_name TEXT NOT NULL,
+  sets REAL,
+  reps REAL,
+  weight REAL,
+  CONSTRAINT fk_rawdaydata
+    FOREIGN KEY(userid, date)
+    REFERENCES RawDayData(userid, date)
+    ON DELETE CASCADE
+);
+"""
+
+create_measurements_table = """
+CREATE TABLE IF NOT EXISTS Measurements (
+  userid text NOT NULL,
+  date text NOT NULL,
+  measure_name text NOT NULL,
+  value REAL NOT NULL,
+  PRIMARY KEY(userid, date, measure_name)
+);
+"""
